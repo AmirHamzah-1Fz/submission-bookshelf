0ac268e0470e8a0c77279512aac250cbf15aec6c
AmirHamzah133 <amirnation23@gmail.com>
2025-03-01 13:27:19 +0700

    Fix ikon SVG yang hilang

--- main.js
+++ main.js
@@ -121,6 +121,7 @@ function createBookElement(bookData) {
   deleteButton.setAttribute('data-testid', 'bookItemDeleteButton');
   deleteButton.classList.add('delete-book-list');
   deleteButton.innerHTML = `
+    <svg xmlns="http://www.w3.org/2000/svg" height="18" viewBox="0 -960 960 960" width="18" fill="#ffffff">
       <path d="M280-120q-33 0-56.5-23.5T200-200v-520q-17 0-28.5-11.5T160-760q0-17 11.5-28.5T200-800h160q0-17 11.5-28.5T400-840h160q17 0 28.5 11.5T600-800h160q17 0 28.5 11.5T800-760q0 17-11.5 28.5T760-720v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM400-280q17 0 28.5-11.5T440-320v-280q0-17-11.5-28.5T400-640q-17 0-28.5 11.5T360-600v280q0 17 11.5 28.5T400-280Zm160 0q17 0 28.5-11.5T600-320v-280q0-17-11.5-28.5T560-640q-17 0-28.5 11.5T520-600v280q0 17 11.5 28.5T560-280ZM280-720v520-520Z"/>
     </svg>
     Hapus Buku`;
@@ -134,6 +135,7 @@ function createBookElement(bookData) {
   editButton.setAttribute('data-testid', 'bookItemEditButton');
   editButton.classList.add('edit-book');
   editButton.innerHTML = `
+    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit">
       <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
       <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
     </svg>
